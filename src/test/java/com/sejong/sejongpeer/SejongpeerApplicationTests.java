d1924d4d83e25030a598944b520ba89e7c64e7ad
ybchar <uiurihappy@sju.ac.kr>
2024-01-09 15:46:52 +0900

    project init

---
+++ src/test/java/com/sejong/sejongpeer/SejongpeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejong.sejongpeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejongpeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
