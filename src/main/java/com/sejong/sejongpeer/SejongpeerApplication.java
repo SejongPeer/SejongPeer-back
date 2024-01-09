0d0f5bed0ecc218f5290f939b01b41b85c26b6ba
ybchar <uiurihappy@sju.ac.kr>
2024-01-09 15:46:52 +0900

    project init

---
+++ src/main/java/com/sejong/sejongpeer/SejongpeerApplication.java
@@ -0,0 +1,13 @@
+package com.sejong.sejongpeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejongpeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejongpeerApplication.class, args);
+	}
+
+}
