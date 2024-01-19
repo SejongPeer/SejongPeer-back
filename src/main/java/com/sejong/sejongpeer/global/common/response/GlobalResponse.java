a88f2f6d2627f6da8d4f8c9e900a4b21154dcb80
ybchar <uiurihappy@sju.ac.kr>
2024-01-19 16:13:48 +0900

    :safety_vest: 전역 처리를 위한 RestControllerAdvice 추가

---
+++ src/main/java/com/sejong/sejongpeer/global/common/response/GlobalResponse.java
@@ -0,0 +1,14 @@
+package com.sejong.sejongpeer.global.common.response;
+
+import com.sejong.sejongpeer.global.error.ErrorResponse;
+import java.time.LocalDateTime;
+
+public record GlobalResponse(boolean success, int status, Object data, LocalDateTime timestamp) {
+    public static GlobalResponse success(int status, Object data) {
+        return new GlobalResponse(true, status, data, LocalDateTime.now());
+    }
+
+    public static GlobalResponse fail(int status, ErrorResponse errorResponse) {
+        return new GlobalResponse(false, status, errorResponse, LocalDateTime.now());
+    }
+}
