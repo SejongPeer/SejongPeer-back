32ddd6af5c57101ebe13d20654178d3b933717ff
swandevson <shai4956@gmail.com>
2024-01-19 19:49:26 +0900

    :sparkles: User 도메인 구현

--- src/main/java/com/sejong/sejongpeer/SejongpeerApplication.java
+++ src/main/java/com/sejong/sejongpeer/SejongpeerApplication.java
@@ -2,11 +2,13 @@ package com.sejong.sejongpeer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SejongpeerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SejongpeerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SejongpeerApplication.class, args);
+	}
 }
