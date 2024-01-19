d21b20aee5970a7afec3524ed2eb6e50d540e481
ybchar <uiurihappy@sju.ac.kr>
2024-01-19 14:43:40 +0900

    chore: spotless 의존성 추가

--- src/main/java/com/sejong/sejongpeer/SejongpeerApplication.java
+++ src/main/java/com/sejong/sejongpeer/SejongpeerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SejongpeerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SejongpeerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SejongpeerApplication.class, args);
+    }
 }
