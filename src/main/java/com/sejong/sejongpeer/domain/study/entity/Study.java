045400d55c81e642585713a821b59ae138bb69a8
ybchar <uiurihappy@sju.ac.kr>
2024-01-26 17:22:00 +0900

    :sparkles: Study Entity 도메인 추가

---
+++ src/main/java/com/sejong/sejongpeer/domain/study/entity/Study.java
@@ -0,0 +1,89 @@
+package com.sejong.sejongpeer.domain.study.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.Comment;
+
+import com.sejong.sejongpeer.domain.common.BaseAuditEntity;
+import com.sejong.sejongpeer.domain.member.entity.Member;
+import com.sejong.sejongpeer.domain.study.entity.type.RecruitmentStatus;
+import com.sejong.sejongpeer.domain.study.entity.type.StudyType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Study extends BaseAuditEntity {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Comment("스터디 제목")
+	@Column(length = 50, nullable = false)
+	private String title;
+
+	@Comment("스터디 내용")
+	@Column(columnDefinition = "text", nullable = false)
+	private String content;
+
+	@Comment("모집 인원")
+	private Integer recruitmentCount;
+
+	@Comment("스터디 유형")
+	@Enumerated(EnumType.STRING)
+	private StudyType type;
+
+	@Comment("스터디 모집 상태")
+	@Enumerated(EnumType.STRING)
+	private RecruitmentStatus recruitmentStatus;
+
+	@Comment("모집 시작 기간")
+	private LocalDateTime recruitmentStartAt;
+
+	@Comment("모집 마감 기간")
+	private LocalDateTime recruitmentEndAt;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@Builder(access = AccessLevel.PRIVATE)
+	private Study(String title, String content, Integer recruitmentCount, StudyType type, RecruitmentStatus recruitmentStatus, LocalDateTime recruitmentStartAt, LocalDateTime recruitmentEndAt, Member member) {
+		this.title = title;
+		this.content = content;
+		this.recruitmentCount = recruitmentCount;
+		this.type = type;
+		this.recruitmentStatus = recruitmentStatus;
+		this.recruitmentStartAt = recruitmentStartAt;
+		this.recruitmentEndAt = recruitmentEndAt;
+		this.member = member;
+	}
+
+	public static Study createStudy(String title, String content, Integer recruitmentCount, StudyType type,
+		LocalDateTime recruitmentStartAt, LocalDateTime recruitmentEndAt, Member member) {
+		return Study.builder()
+			.title(title)
+			.content(content)
+			.recruitmentCount(recruitmentCount)
+			.type(type)
+			.recruitmentStatus(RecruitmentStatus.RECRUITING)
+			.recruitmentStartAt(recruitmentStartAt)
+			.recruitmentEndAt(recruitmentEndAt)
+			.member(member)
+			.build();
+	}
+}
---
+++ src/main/java/com/sejong/sejongpeer/domain/study/entity/type/RecruitmentStatus.java
@@ -0,0 +1,14 @@
+package com.sejong.sejongpeer.domain.study.entity.type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum RecruitmentStatus {
+	RECRUITING("모집 중"),
+	DEADLINE("마감")
+	;
+
+	private final String value;
+}
