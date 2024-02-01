9f5c60e743322ffd23a1c37c5391a4d9a5931bf5
swandevson <shai4956@gmail.com>
2024-02-02 06:49:11 +0900

    :white_check_mark: 회원가입 테스트 코드 수정 및 PasswordEncoder Config 추가

--- src/test/java/com/sejong/sejongpeer/domain/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/sejong/sejongpeer/domain/member/repository/MemberRepositoryTest.java
@@ -1,48 +1,57 @@
 package com.sejong.sejongpeer.domain.member.repository;
 
-import com.sejong.sejongpeer.domain.member.entity.Member;
-import com.sejong.sejongpeer.domain.member.entity.type.Gender;
-import com.sejong.sejongpeer.domain.member.entity.type.Status;
-import com.sejong.sejongpeer.global.config.JpaAuditingConfig;
 import java.time.LocalDate;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.sejong.sejongpeer.config.PasswordEncoderTestConfig;
+import com.sejong.sejongpeer.domain.member.entity.Member;
+import com.sejong.sejongpeer.domain.member.entity.type.Gender;
+import com.sejong.sejongpeer.domain.member.entity.type.Status;
+import com.sejong.sejongpeer.global.config.JpaAuditingConfig;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Import(JpaAuditingConfig.class)
+@Import({JpaAuditingConfig.class, PasswordEncoderTestConfig.class})
 class MemberRepositoryTest {
-    @Autowired private MemberRepository memberRepository;
+	@Autowired
+	private MemberRepository memberRepository;
 
-    @Test
-    void 회원가입_유저를_저장한다() {
-        // given
-        Member member =
-                Member.builder()
-                        .phoneNumber("01012345678")
-                        .grade(1)
-                        .gender(Gender.MALE)
-                        .studentId("12345678")
-                        .name("홍길동")
-                        .email("test@test.ac.kr")
-                        .birthday(LocalDate.of(1999, 1, 1))
-                        .major("computer science")
-                        .account("test")
-                        .password("test")
-                        .build();
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-        // when
-        Member savedMember = memberRepository.save(member);
+	@Test
+	void 회원가입_유저를_저장한다() {
+		String encryptedPassword = passwordEncoder.encode("test");
+		// given
+		Member member =
+			Member.builder()
+				.phoneNumber("01012341234")
+				.grade(1)
+				.gender(Gender.MALE)
+				.studentId("12345678")
+				.name("홍길동")
+				.email("test@test.ac.kr")
+				.birthday(LocalDate.of(1999, 1, 1))
+				.major("computer science")
+				.account("test")
+				.password(encryptedPassword)
+				.build();
 
-        // then
-        Assertions.assertNotNull(savedMember);
-        Assertions.assertNotNull(savedMember.getId());
-        Assertions.assertNotNull(savedMember.getCreatedAt());
-        Assertions.assertNotNull(savedMember.getUpdatedAt());
-        Assertions.assertEquals(member.getStatus(), Status.ACTIVE);
-    }
+		// when
+		Member savedMember = memberRepository.save(member);
+
+		// then
+		Assertions.assertNotNull(savedMember);
+		Assertions.assertNotNull(savedMember.getId());
+		Assertions.assertNotNull(savedMember.getCreatedAt());
+		Assertions.assertNotNull(savedMember.getUpdatedAt());
+		Assertions.assertEquals(member.getStatus(), Status.ACTIVE);
+	}
 }
