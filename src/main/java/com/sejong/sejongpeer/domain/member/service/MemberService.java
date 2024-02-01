c02dd7670e4d69e8f3fec50d814115395920e54b
swandevson <shai4956@gmail.com>
2024-02-02 08:51:31 +0900

    :sparkles: 회원가입 시 예외처리

--- src/main/java/com/sejong/sejongpeer/domain/member/service/MemberService.java
+++ src/main/java/com/sejong/sejongpeer/domain/member/service/MemberService.java
@@ -7,6 +7,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.sejong.sejongpeer.domain.member.dto.SignUpRequest;
 import com.sejong.sejongpeer.domain.member.entity.Member;
 import com.sejong.sejongpeer.domain.member.repository.MemberRepository;
+import com.sejong.sejongpeer.global.error.exception.CustomException;
+import com.sejong.sejongpeer.global.error.exception.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,13 +21,50 @@ public class MemberService {
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
 
-    public void signUp(SignUpRequest request) { // TODO: 이메일, 학번 중복 여부 체크
-        String encodedPassword = passwordEncoder.encode(request.password());
-        Member member = Member.create(request, encodedPassword);
+	public void signUp(SignUpRequest request) {
+		verifySignUp(request);
 
-        RefreshToken refreshToken = RefreshToken.builder().member(member).token("").build();
-        refreshTokenRepository.save(refreshToken);
+		String encodedPassword = passwordEncoder.encode(request.password());
+		Member member = Member.create(request, encodedPassword);
 
-        memberRepository.save(member);
-    }
+		memberRepository.save(member);
+	}
+
+	private void verifySignUp(SignUpRequest request) {
+		if (!request.password().equals(request.passwordCheck())) {
+			throw new CustomException(ErrorCode.PASSWORD_NOT_MATCH);
+		}
+		if (isPhoneNumberExists(request.phoneNumber())) {
+			throw new CustomException(ErrorCode.DUPLICATED_PHONE_NUMBER);
+		}
+		if (isAccountExists(request.account())) {
+			throw new CustomException(ErrorCode.DUPLICATED_ACCOUNT);
+		}
+		if (isStudentIdExists(request.studentId())) {
+			throw new CustomException(ErrorCode.DUPLICATED_STUDENT_ID);
+		}
+		if (isEmailExists(request.email())) {
+			throw new CustomException(ErrorCode.DUPLICATED_EMAIL);
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public boolean isEmailExists(String email) {
+		return memberRepository.existsByEmail(email);
+	}
+
+	@Transactional(readOnly = true)
+	public boolean isStudentIdExists(String studentId) {
+		return memberRepository.existsByStudentId(studentId);
+	}
+
+	@Transactional(readOnly = true)
+	public boolean isAccountExists(String account) {
+		return memberRepository.existsByAccount(account);
+	}
+
+	@Transactional(readOnly = true)
+	public boolean isPhoneNumberExists(String phoneNumber) {
+		return memberRepository.existsByPhoneNumber(phoneNumber);
+	}
 }
--- src/main/java/com/sejong/sejongpeer/global/error/exception/ErrorCode.java
+++ src/main/java/com/sejong/sejongpeer/global/error/exception/ErrorCode.java
@@ -1,19 +1,29 @@
 package com.sejong.sejongpeer.global.error.exception;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    SAMPLE_ERROR(HttpStatus.BAD_REQUEST, "Sample Error Message"),
-    // 서버 에러
-    METHOD_ARGUMENT_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "Enum Type이 일치하지 않아 Binding에 실패하였습니다."),
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP method 입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류, 관리자에게 문의하세요"),
-    ;
+	SAMPLE_ERROR(HttpStatus.BAD_REQUEST, "Sample Error Message"),
+	// 서버 에러
+	METHOD_ARGUMENT_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "Enum Type이 일치하지 않아 Binding에 실패하였습니다."),
+	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP method 입니다."),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류, 관리자에게 문의하세요"),
 
-    private final HttpStatus status;
-    private final String message;
+	// 회원가입 에러
+	SIGN_UP_ERROR(HttpStatus.BAD_REQUEST, "회원가입에 실패하였습니다."),
+	PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+	DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
+	DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임입니다."),
+	DUPLICATED_STUDENT_ID(HttpStatus.BAD_REQUEST, "이미 존재하는 학번입니다."),
+	DUPLICATED_PHONE_NUMBER(HttpStatus.BAD_REQUEST, "이미 존재하는 전화번호입니다."),
+	DUPLICATED_ACCOUNT(HttpStatus.BAD_REQUEST, "이미 존재하는 계정입니다."),
+	;
+
+	private final HttpStatus status;
+	private final String message;
 }
