4e723e2b9b57747c8d68de12ce7913a3db63d236
swandevson <shai4956@gmail.com>
2024-02-02 09:05:53 +0900

    :memo: 누락된 swagger 추가

--- src/main/java/com/sejong/sejongpeer/domain/member/api/MemberController.java
+++ src/main/java/com/sejong/sejongpeer/domain/member/api/MemberController.java
@@ -8,15 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sejong.sejongpeer.domain.member.dto.SignUpRequest;
 import com.sejong.sejongpeer.domain.member.service.MemberService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "2. [회원]", description = "회원 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/member")
 public class MemberController {
 	private final MemberService memberService;
 
+	@Operation(summary = "회원가입", description = "회원가입을 진행합니다.")
 	@PostMapping("/sign-up")
 	public void signUp(@Valid @RequestBody SignUpRequest request) {
 		memberService.signUp(request);
