0972b011e05c525f9b77d1497d113d923e28d828
ybchar <uiurihappy@sju.ac.kr>
2024-02-06 17:58:55 +0900

    feat: 스터디 리스트 조회 v1

--- src/main/java/com/sejong/sejongpeer/domain/study/repository/StudyRepositoryImpl.java
+++ src/main/java/com/sejong/sejongpeer/domain/study/repository/StudyRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.sejong.sejongpeer.domain.study.repository;
 
+import static com.sejong.sejongpeer.domain.study.entity.QStudy.*;
+
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sejong.sejongpeer.domain.study.entity.Study;
 
 import lombok.RequiredArgsConstructor;
@@ -18,7 +22,19 @@ public class StudyRepositoryImpl implements StudyRepositoryCustom{
 
 	@Override
 	public Slice<Study> findStudySlice(int size, Long lastId) {
-		return ;
+		List<Study> studies = jpaQueryFactory.selectFrom(study)
+			.where(ltStudyId(lastId))
+			.orderBy(study.id.desc())
+			.limit(size + 1)
+			.fetch();
+		return checkLastPage(size, studies);
+	}
+
+	private BooleanExpression ltStudyId(Long lastId) {
+		if (lastId == null) {
+			return null;
+		}
+		return study.id.lt(lastId);
 	}
 
 	private Slice<Study> checkLastPage(
