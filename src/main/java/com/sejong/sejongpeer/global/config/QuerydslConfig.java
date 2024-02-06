b65ec407121c23bda311a4faccd579b549960c28
ybchar <uiurihappy@sju.ac.kr>
2024-02-06 16:53:55 +0900

    fix: spotlessApply

--- src/main/java/com/sejong/sejongpeer/global/config/QuerydslConfig.java
+++ src/main/java/com/sejong/sejongpeer/global/config/QuerydslConfig.java
@@ -1,21 +1,17 @@
 package com.sejong.sejongpeer.global.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.query.JpaQueryMethodFactory;
-
 import com.querydsl.jpa.impl.JPAQueryFactory;
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QuerydslConfig {
-	@PersistenceContext
-	public EntityManager entityManager;
+    @PersistenceContext public EntityManager entityManager;
 
-	@Bean
-	public JPAQueryFactory jpaQueryFactory() {
-		return new JPAQueryFactory(entityManager);
-	}
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
 }
