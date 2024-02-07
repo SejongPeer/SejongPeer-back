f01a7ba5e7847b9d03032be8d07ad097139122d3
ybchar <uiurihappy@sju.ac.kr>
2024-02-07 11:20:11 +0900

    fix: study id Long으로 롤백

--- src/main/java/com/sejong/sejongpeer/domain/study/entity/Study.java
+++ src/main/java/com/sejong/sejongpeer/domain/study/entity/Study.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,7 +20,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
-import org.hibernate.annotations.UuidGenerator;
 
 @Getter
 @Entity
@@ -26,8 +27,7 @@ import org.hibernate.annotations.UuidGenerator;
 public class Study extends BaseAuditEntity {
 
     @Id
-    @UuidGenerator
-    @Column(columnDefinition = "char(36)")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Comment("스터디 제목")
