4c94596f94db534444924ed6e6fc19b9d3449698
haiseong <wjdgotjd9908@gmail.com>
2024-02-18 09:52:00 +0900

    :sparkles: 댓글 Repository 생성

---
+++ src/main/java/com/sejong/sejongpeer/domain/comment/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.sejong.sejongpeer.domain.comment.repository;
+
+import com.sejong.sejongpeer.domain.comment.entity.Comment;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, String> {
+	List<Comment> findAllByStudyId(String studyId);
+}
