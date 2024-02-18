c9df664c0e1e1c9f200091215628a2454cc4caa4
haiseong <wjdgotjd9908@gmail.com>
2024-02-18 12:52:44 +0900

    :fire: 필요없는 dto 삭제

--- src/main/java/com/sejong/sejongpeer/domain/comment/dto/request/CommentPostRequest.java
+++
@@ -1,8 +0,0 @@
-package com.sejong.sejongpeer.domain.comment.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record CommentPostRequest(
-	@NotBlank String content,
-	Long parentId) {
-}
