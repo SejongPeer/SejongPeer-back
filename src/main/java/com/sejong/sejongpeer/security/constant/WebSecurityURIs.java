608048c68478a534b2a21ff37426259fee1c57cc
swandevson <shai4956@gmail.com>
2024-02-18 14:53:45 +0900

    :gear: 1.[스터디] -> 3.[스터디]로 변경

--- src/main/java/com/sejong/sejongpeer/security/constant/WebSecurityURIs.java
+++ src/main/java/com/sejong/sejongpeer/security/constant/WebSecurityURIs.java
@@ -1,16 +1,18 @@
 package com.sejong.sejongpeer.security.constant;
 
 import java.util.List;
+
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public final class WebSecurityURIs {
-    public static final List<String> PUBLIC_URIS =
-            List.of(
-                    "/api/v1/auth/sign-in",
-                    "/api/v1/member/sign-up",
-                    "/api/v1/member/check-account",
-                    "/api/v1/member/check-nickname",
-                    "/api/v1/member/help/find-account",
-                    "/api/v1/member/help/reset-password");
+	public static final List<String> PUBLIC_URIS =
+		List.of(
+			"/api/v1/auth/sign-in",
+			"/api/v1/member/sign-up",
+			"/api/v1/member/check-account",
+			"/api/v1/member/check-nickname",
+			"/api/v1/member/help/find-account",
+			"/api/v1/member/help/reset-password",
+			"/swagger-ui/**");
 }
