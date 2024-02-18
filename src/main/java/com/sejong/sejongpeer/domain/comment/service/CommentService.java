0399fa153af2fdbc489380ddac14c99a0a55ca7f
haiseong <wjdgotjd9908@gmail.com>
2024-02-18 09:52:29 +0900

    :sparkles: 댓글 Service 댓글 가져오기 기능 작성

---
+++ src/main/java/com/sejong/sejongpeer/domain/comment/service/CommentService.java
@@ -0,0 +1,56 @@
+package com.sejong.sejongpeer.domain.comment.service;
+
+import com.sejong.sejongpeer.domain.comment.dto.response.CommentResponse;
+import com.sejong.sejongpeer.domain.comment.entity.Comment;
+import com.sejong.sejongpeer.domain.comment.repository.CommentRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+@Service
+public class CommentService {
+	private final CommentRepository commentRepository;
+
+	@Transactional(readOnly = true)
+	public List<CommentResponse> getComments(String studyId) {
+		List<Comment> comments = commentRepository.findAllByStudyId(studyId);
+
+		Map<Long, CommentResponse> commentResponseMap = comments.stream()
+			.filter(comment -> comment.getParent() == null)
+			.collect(Collectors.toMap(
+				Comment::getId,
+				comment -> new CommentResponse(
+					comment.getId(),
+					comment.getMember().getNickname(),
+					comment.getContent(),
+					new ArrayList<>()
+				)
+			));
+
+		comments.stream()
+			.filter(comment -> comment.getParent() != null)
+			.forEach(comment ->
+				commentResponseMap.get(comment.getParent().getId())
+					.subComments()
+					.add(
+						new CommentResponse(
+							comment.getId(),
+							comment.getMember().getNickname(),
+							comment.getContent(),
+							null)
+					)
+			);
+
+		return comments.stream()
+			.map(comment -> commentResponseMap.get(comment.getId()))
+			.collect(Collectors.toList());
+	}
+}
