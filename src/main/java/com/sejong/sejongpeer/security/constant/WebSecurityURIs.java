f150d072b3d0bbbedeba4337bd3e208ce44ffc06
ybchar <uiurihappy@sju.ac.kr>
2024-02-20 17:42:59 +0900

    fix: PUBLIC URLS 롤백

--- src/main/java/com/sejong/sejongpeer/security/constant/WebSecurityURIs.java
+++ src/main/java/com/sejong/sejongpeer/security/constant/WebSecurityURIs.java
@@ -8,10 +8,12 @@ public final class WebSecurityURIs {
     public static final List<String> PUBLIC_URIS =
             List.of(
                     "/api/v1/auth/sign-in",
-                    "/api/v1/member/**",
-                    "/api/v1/member/help/**",
-                    "/api/v1/study/**",
+                    "/api/v1/member/sign-up",
+                    "/api/v1/member/check-account",
+                    "/api/v1/member/check-nickname",
+                    "/api/v1/member/help/find-account",
+                    "/api/v1/member/help/reset-password",
                     "/swagger-ui/**");
     public static final List<String> CORS_ALLOW_URIS =
-            List.of("http://localhost:3000", "https://sejongpeer.com");
+            List.of("http://localhost:3000", "http://127.0.0.1:3000", "https://sejongpeer.com");
 }
