9db7772c27b715a70f039e3b56fa6b39b726f0ad
swandevson <shai4956@gmail.com>
2024-02-21 23:32:49 +0900

    :bug: spotlessApply

--- src/main/java/com/sejong/sejongpeer/security/constant/WebSecurityURIs.java
+++ src/main/java/com/sejong/sejongpeer/security/constant/WebSecurityURIs.java
@@ -1,23 +1,21 @@
 package com.sejong.sejongpeer.security.constant;
 
 import java.util.List;
-
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public final class WebSecurityURIs {
-	public static final List<String> PUBLIC_URIS =
-		List.of(
-			"/api/v1/auth/sign-in",
-			"/api/v1/member/sign-up",
-			"/api/v1/member/check-account",
-			"/api/v1/member/check-nickname",
-			"/api/v1/member/check-kakao-account",
-			"/api/v1/member/check-phone-number",
-			"/api/v1/member/help/find-account",
-			"/api/v1/member/help/reset-password",
-
-			"/swagger-ui/**");
-	public static final List<String> CORS_ALLOW_URIS =
-		List.of("http://localhost:3000", "http://127.0.0.1:3000", "https://sejongpeer.com");
+    public static final List<String> PUBLIC_URIS =
+            List.of(
+                    "/api/v1/auth/sign-in",
+                    "/api/v1/member/sign-up",
+                    "/api/v1/member/check-account",
+                    "/api/v1/member/check-nickname",
+                    "/api/v1/member/check-kakao-account",
+                    "/api/v1/member/check-phone-number",
+                    "/api/v1/member/help/find-account",
+                    "/api/v1/member/help/reset-password",
+                    "/swagger-ui/**");
+    public static final List<String> CORS_ALLOW_URIS =
+            List.of("http://localhost:3000", "http://127.0.0.1:3000", "https://sejongpeer.com");
 }
