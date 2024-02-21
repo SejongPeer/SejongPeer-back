a13d6803d4a570064e868cddad79cc15aef472e1
swandevson <shai4956@gmail.com>
2024-02-21 22:32:15 +0900

    :sparkles: 휴대폰 번호, 카카오 계정 중복체크 구현 및 중복체크 리팩토링

--- src/main/java/com/sejong/sejongpeer/domain/member/dto/response/AccountFindResponse.java
+++ src/main/java/com/sejong/sejongpeer/domain/member/dto/response/AccountFindResponse.java
@@ -1,3 +1,7 @@
 package com.sejong.sejongpeer.domain.member.dto.response;
 
-public record AccountFindResponse(String account) {}
+public record AccountFindResponse(String account) {
+    public static AccountFindResponse of(String account) {
+        return new AccountFindResponse(account);
+    }
+}
