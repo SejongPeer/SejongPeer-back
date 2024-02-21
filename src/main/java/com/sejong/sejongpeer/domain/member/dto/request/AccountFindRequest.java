be0e3a4fe9043afcd30f32be0bab141bc8d661c9
swandevson <shai4956@gmail.com>
2024-02-21 22:32:36 +0900

    :bug: 휴대폰 번호 정규식 수정

--- src/main/java/com/sejong/sejongpeer/domain/member/dto/request/AccountFindRequest.java
+++ src/main/java/com/sejong/sejongpeer/domain/member/dto/request/AccountFindRequest.java
@@ -9,5 +9,5 @@ import jakarta.validation.constraints.Size;
 public record AccountFindRequest(
         @NotNull(message = "계정 찾기 시 인증할 수단을 선택해주세요") AccountFindOption option,
         @NotBlank(message = "학번은 필수 입력값입니다.") String studentId,
-        @Pattern(regexp = "^010[0-9]{7}$", message = "올바른 전화번호를 입력해주세요.") String phoneNumber,
+        @Pattern(regexp = "^010[0-9]{8}$", message = "올바른 전화번호를 입력해주세요.") String phoneNumber,
         @Size(min = 2, message = "이름은 최소 2자 이상 입력해주세요.") String name) {}
