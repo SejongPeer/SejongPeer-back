06031e69b821dc9ec3ebd6f6596d83594c208e5b
swandevson <shai4956@gmail.com>
2024-02-22 23:45:04 +0900

    :wrench: application-infra.yml 추가

--- src/test/java/com/sejong/sejongpeer/SejongpeerApplicationTests.java
+++ src/test/java/com/sejong/sejongpeer/SejongpeerApplicationTests.java
@@ -2,10 +2,8 @@ package com.sejong.sejongpeer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootTest
-@PropertySource("classpath:application.properties")
 class SejongpeerApplicationTests {
 
 	@Test
