a8086ee7e5460b6774da4323effe9d0ca4833d25
rong5026 <rong5026@naver.com>
2024-02-22 19:13:05 +0900

    :zap: range 변수명, type 변수명 변경, builder isSubMajor 인자 추가

--- src/main/java/com/sejong/sejongpeer/domain/buddy/entity/Buddy.java
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/entity/Buddy.java
@@ -3,11 +3,13 @@ package com.sejong.sejongpeer.domain.buddy.entity;
 import com.sejong.sejongpeer.domain.buddy.entity.type.*;
 import com.sejong.sejongpeer.domain.common.BaseAuditEntity;
 import com.sejong.sejongpeer.domain.member.entity.Member;
+
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import org.hibernate.annotations.Comment;
 
 @Entity
@@ -15,73 +17,77 @@ import org.hibernate.annotations.Comment;
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Buddy extends BaseAuditEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
 
-    @Comment("버디 성별")
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private GenderOption genderOption;
+	@Comment("버디 성별")
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private GenderOption genderOption;
 
-    @Comment("버디 타입")
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private BuddyType buddyType;
+	@Comment("버디 타입")
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private BuddyType classType;
 
-    @Comment("버디 범위")
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private BuddyRange buddyRange;
+	@Comment("버디 범위")
+	@Enumerated(EnumType.STRING)
+	@Column(name = "`range`", nullable = false)
+	private BuddyRange range;
 
-    @Comment("버디 학년 범위")
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private GradeOption gradeOption;
+	@Comment("버디 학년 범위")
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private GradeOption gradeOption;
 
-    @Comment("매칭 상태")
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private MatchingStatus matchingStatus;
+	@Comment("매칭 상태")
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private MatchingStatus matchingStatus;
 
-    @Comment("복수전공 확인")
-    @Column(nullable = false)
-    private boolean isSubMajor;
+	@Comment("복수전공 확인")
+	@Column(nullable = false)
+	private boolean isSubMajor;
 
-    @Builder(access = AccessLevel.PRIVATE)
-    private Buddy(
-            Member member,
-            GenderOption genderOption,
-            BuddyType buddyType,
-            BuddyRange buddyRange,
-            GradeOption gradeOption,
-            MatchingStatus matchingStatus) {
-        this.member = member;
-        this.genderOption = genderOption;
-        this.buddyType = buddyType;
-        this.buddyRange = buddyRange;
-        this.gradeOption = gradeOption;
-        this.matchingStatus = matchingStatus;
-    }
+	@Builder(access = AccessLevel.PRIVATE)
+	private Buddy(
+		Member member,
+		GenderOption genderOption,
+		BuddyType classType,
+		BuddyRange range,
+		GradeOption gradeOption,
+		MatchingStatus matchingStatus,
+		boolean isSubMajor) {
+		this.member = member;
+		this.genderOption = genderOption;
+		this.classType = classType;
+		this.range = range;
+		this.gradeOption = gradeOption;
+		this.matchingStatus = matchingStatus;
+		this.isSubMajor = isSubMajor;
+	}
 
-    public static Buddy createBuddy(
-            Member member,
-            GenderOption genderOption,
-            BuddyType buddyType,
-            BuddyRange buddyRange,
-            GradeOption gradeOption,
-            MatchingStatus matchingStatus) {
-        return Buddy.builder()
-                .member(member)
-                .genderOption(genderOption)
-                .buddyType(buddyType)
-                .buddyRange(buddyRange)
-                .gradeOption(gradeOption)
-                .matchingStatus(matchingStatus)
-                .build();
-    }
+	public static Buddy createBuddy(
+		Member member,
+		GenderOption genderOption,
+		BuddyType classType,
+		BuddyRange range,
+		GradeOption gradeOption,
+		MatchingStatus matchingStatus,
+		boolean isSubMajor) {
+		return Buddy.builder()
+			.member(member)
+			.genderOption(genderOption)
+			.classType(classType)
+			.range(range)
+			.gradeOption(gradeOption)
+			.matchingStatus(matchingStatus)
+			.isSubMajor(isSubMajor)
+			.build();
+	}
 }
