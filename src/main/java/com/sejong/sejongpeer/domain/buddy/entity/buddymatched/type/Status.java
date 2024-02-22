27235833bbb8ca5b2ca99d5cb891cd2314f5b551
rong5026 <rong5026@naver.com>
2024-02-22 21:59:09 +0900

    :art: Buddy, BuddyMatched 엔티티 패키지 분

--- src/main/java/com/sejong/sejongpeer/domain/buddy/entity/buddymatched/type/Status.java
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/entity/buddymatched/type/Status.java
@@ -1,11 +1,11 @@
-package com.sejong.sejongpeer.domain.buddy.entity.type;
+package com.sejong.sejongpeer.domain.buddy.entity.buddymatched.type;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum BuddyMatchedStatus {
+public enum Status {
     IN_PROGRESS("찾는 중"),
     MATCHING_COMPLETED("매칭 성사"),
     MATCHING_FAIL("매칭 실패");
