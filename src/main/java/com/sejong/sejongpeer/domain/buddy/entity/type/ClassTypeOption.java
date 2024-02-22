2383bd3621a565581a6bd9b8375cd6da52349a07
rong5026 <rong5026@naver.com>
2024-02-22 21:11:17 +0900

    :art: range 변수명, type 변수명 변경

---
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/entity/type/ClassTypeOption.java
@@ -0,0 +1,14 @@
+package com.sejong.sejongpeer.domain.buddy.entity.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ClassTypeOption {
+	SENIOR("선배"),
+	JUNIOR("후배"),
+	MATE("동기"),
+	NO_MATTER("상관없음");
+	private final String value;
+}
