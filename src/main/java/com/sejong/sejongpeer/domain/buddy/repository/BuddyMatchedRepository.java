0ddd5b7a10f6a3a6011ce8444e9ab7371017a630
Subin <rla0010101@gmail.com>
2024-02-23 14:23:00 +0900

    :sparkles: BuddyMatchedRepository

---
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/repository/BuddyMatchedRepository.java
@@ -0,0 +1,7 @@
+package com.sejong.sejongpeer.domain.buddy.repository;
+
+import com.sejong.sejongpeer.domain.buddy.entity.BuddyMatched;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuddyMatchedRepository extends JpaRepository<BuddyMatched, Long> {
+}
