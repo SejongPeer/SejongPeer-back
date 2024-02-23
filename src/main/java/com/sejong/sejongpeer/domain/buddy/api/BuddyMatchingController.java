3e67eefb66d11fa75034bfa09973f965c992d38f
Subin <rla0010101@gmail.com>
2024-02-23 14:18:33 +0900

    :sparkles: BuddyMatchingController

---
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/api/BuddyMatchingController.java
@@ -0,0 +1,27 @@
+package com.sejong.sejongpeer.domain.buddy.api;
+
+import com.sejong.sejongpeer.domain.buddy.service.BuddyMatchingService;
+import com.sejong.sejongpeer.domain.buddy.dto.request.BuddyMatchingStatusUpdateRequest;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "5. [버디 매칭]", description = "세종버디 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/buddyMatching")
+public class BuddyMatchingController {
+	private final BuddyMatchingService buddyMatchingService;
+
+	@Operation(summary = "버디 매칭 상태 업데이트", description = "버디 매칭 상태 관리")
+	@PostMapping("/status")
+	public ResponseEntity<String> checkBuddyMatching(@RequestBody BuddyMatchingStatusUpdateRequest request) {
+		buddyMatchingService.updateBuddyMatchingStatus(request);
+		return ResponseEntity.ok("BuddyMatched status updated successfully");
+	}
+}
