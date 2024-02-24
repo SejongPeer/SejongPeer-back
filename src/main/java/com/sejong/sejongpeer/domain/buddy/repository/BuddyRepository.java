97f4d1e76184d1edcf188fdbdbb8caa8ca693923
rong5026 <rong5026@naver.com>
2024-02-24 15:28:32 +0900

    :art: BuddyRepository Limit 1 추가

--- src/main/java/com/sejong/sejongpeer/domain/buddy/repository/BuddyRepository.java
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/repository/BuddyRepository.java
@@ -13,7 +13,6 @@ import com.sejong.sejongpeer.domain.buddy.entity.buddy.type.Status;
 public interface BuddyRepository extends JpaRepository<Buddy, Long> {
 	List<Buddy> findByStatus(Status status);
 
-	@Query("SELECT b FROM Buddy b WHERE b.member.id = :memberId ORDER BY b.id DESC")
+	@Query("SELECT b FROM Buddy b WHERE b.member.id = :memberId ORDER BY b.id DESC LIMIT 1")
 	Optional<Buddy> findLastBuddyByMemberId(@Param("memberId") String memberId);
-
 }
