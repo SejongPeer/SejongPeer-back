f11d16be85b88e00d8602235d5888cd28a4a4c72
rong5026 <rong5026@naver.com>
2024-02-24 16:33:18 +0900

    :sparkles: 매치상태 REsponse

---
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/dto/response/MatchingStatusResponse.java
@@ -0,0 +1,6 @@
+package com.sejong.sejongpeer.domain.buddy.dto.response;
+
+import com.sejong.sejongpeer.domain.buddy.entity.buddy.type.Status;
+
+public record MatchingStatusResponse(Status status) {
+}
