d56a4f2f55b691d2651b93fdf0232eee739a5bed
swandevson <shai4956@gmail.com>
2024-02-27 01:17:57 +0900

    :sparkles: 혼밥 매칭 기능 구현

---
+++ src/main/java/com/sejong/sejongpeer/domain/honbab/service/HonbabMatchingService.java
@@ -0,0 +1,85 @@
+package com.sejong.sejongpeer.domain.honbab.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sejong.sejongpeer.domain.honbab.entity.honbab.Honbab;
+import com.sejong.sejongpeer.domain.honbab.entity.honbab.type.HonbabStatus;
+import com.sejong.sejongpeer.domain.honbab.entity.honbabmatched.HonbabMatched;
+import com.sejong.sejongpeer.domain.honbab.repository.HonbabMatchedRepository;
+import com.sejong.sejongpeer.domain.honbab.repository.HonbabRepository;
+import com.sejong.sejongpeer.domain.honbab.util.HonbabFilter;
+import com.sejong.sejongpeer.infra.sms.service.SmsService;
+import com.sejong.sejongpeer.infra.sms.service.SmsText;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Transactional
+@Slf4j
+@RequiredArgsConstructor
+public class HonbabMatchingService {
+	private final SmsService smsService;
+
+	private final HonbabRepository honbabRepository;
+	private final HonbabMatchedRepository honbabMatchedRepository;
+
+	public void executeMatching() {
+		List<Honbab> candidates = honbabRepository.findAllByStatus(HonbabStatus.IN_PROGRESS);
+		List<HonbabMatched> honbabMatcheds = new ArrayList<>();
+
+		for (Honbab me : candidates) {
+			HonbabMatched honbabMatched = matchHonbab(candidates, me);
+			if (honbabMatched != null) {
+				honbabMatcheds.add(honbabMatched);
+			}
+		}
+
+		honbabRepository.saveAll(candidates);    // matchHonbab에서 변경된 상태 벌크로 업데이트
+		honbabMatchedRepository.saveAll(honbabMatcheds);
+	}
+
+	public HonbabMatched matchHonbab(Honbab me) {
+		List<Honbab> candidates = honbabRepository.findAllByStatus(HonbabStatus.IN_PROGRESS);
+
+		return matchHonbab(candidates, me);
+	}
+
+	public HonbabMatched matchHonbab(List<Honbab> candidates, Honbab me) {
+		Honbab partner = findSuitableHonbab(candidates, me);
+
+		if (partner == null) {
+			return null;
+		}
+
+		HonbabMatched honbabMatched = HonbabMatched.registerMatchingPair(me, partner);
+
+		partner.changeStatus(HonbabStatus.MATCHING_COMPLETED);
+		me.changeStatus(HonbabStatus.MATCHING_COMPLETED);
+
+		sendMatchingMessage(me);
+		sendMatchingMessage(partner);
+
+		return honbabMatched;
+	}
+
+	private Honbab findSuitableHonbab(List<Honbab> candidates, Honbab me) {
+		return candidates.stream()
+			.filter(candidate -> candidate.getId() != me.getId())
+			.filter(candidate -> candidate.getStatus() == HonbabStatus.IN_PROGRESS)
+			.filter(candidate -> HonbabFilter.filterSutiableGender(candidate, me))
+			.filter(candidate -> HonbabFilter.filterSuitableMenuCategory(candidate, me))
+			.findFirst()
+			.orElse(null);
+	}
+
+	private void sendMatchingMessage(Honbab me) {
+		String phoneNumber = me.getMember().getPhoneNumber();
+
+		smsService.sendSms(phoneNumber, SmsText.MATCHING_FOUND_HONBAB);
+	}
+}
--- src/main/java/com/sejong/sejongpeer/infra/sms/service/SmsText.java
+++ src/main/java/com/sejong/sejongpeer/infra/sms/service/SmsText.java
@@ -1,7 +1,8 @@
 package com.sejong.sejongpeer.infra.sms.service;
 
 public enum SmsText {
-	MATCHING_FOUND_BUDDY("버디가 매칭되었습니다. 접속하여 확인해주세요!");
+	MATCHING_FOUND_BUDDY("버디가 매칭되었습니다. 접속하여 확인해주세요!"),
+	MATCHING_FOUND_HONBAB("혼밥이 매칭되었습니다. 접속하여 확인해주세요!");
 
 	private final String value;
 
