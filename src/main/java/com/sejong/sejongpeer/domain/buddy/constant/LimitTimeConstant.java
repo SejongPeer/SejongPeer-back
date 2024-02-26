1a06031fbef1c92d8eec361fea417a2b1dfdbd23
rong5026 <rong5026@naver.com>
2024-02-26 23:33:37 +0900

    :art: 패널티 시간 1시간 -> 24시간 , 상수처리

---
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/constant/LimitTimeConstant.java
@@ -0,0 +1,10 @@
+package com.sejong.sejongpeer.domain.buddy.constant;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public final class LimitTimeConstant {
+
+	public static final int ONE_DAY = 24;
+}
+
