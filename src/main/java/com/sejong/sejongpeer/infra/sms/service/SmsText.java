4656b0ceb07c5029c78fc1c7c1450b2a6035a32d
swandevson <shai4956@gmail.com>
2024-02-27 17:34:26 +0900

    :bug: 컴파일 에러 수정

--- src/main/java/com/sejong/sejongpeer/infra/sms/service/SmsText.java
+++ src/main/java/com/sejong/sejongpeer/infra/sms/service/SmsText.java
@@ -2,11 +2,10 @@ package com.sejong.sejongpeer.infra.sms.service;
 
 public enum SmsText {
 	MATCHING_FOUND_HONBAB("[세종피어] 밥짝꿍 매칭에 성공했습니다!\n"
-		+ "혼밥탈출에 다시 접속해 밥짝꿍의 정보를 확인해주세요:)");
+		+ "혼밥탈출에 다시 접속해 밥짝꿍의 정보를 확인해주세요:)"),
 	MATCHING_FOUND_BUDDY("[세종피어] 버디를 찾았습니다! \n"
 		+ "세종버디에 재접속해 버디정보(학과, 학년) 확인 후 수락여부를 결정해주세요."),
 	MATCHING_COMPLETE_BUDDY("[세종피어] 버디 매칭에 성공했습니다! 혼밥탈출에 다시 접속해 밥짝꿍의 정보를 확인해주세요:)");
-
 
 	private final String value;
 
