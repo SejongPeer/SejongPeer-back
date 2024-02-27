7adec116023bfb3a82b6f1848cf9bb007a819d3b
swandevson <shai4956@gmail.com>
2024-02-27 13:17:53 +0900

    :gear: 문자 내용 변경

--- src/main/java/com/sejong/sejongpeer/infra/sms/service/SmsText.java
+++ src/main/java/com/sejong/sejongpeer/infra/sms/service/SmsText.java
@@ -2,7 +2,8 @@ package com.sejong.sejongpeer.infra.sms.service;
 
 public enum SmsText {
 	MATCHING_FOUND_BUDDY("버디가 매칭되었습니다. 접속하여 확인해주세요!"),
-	MATCHING_FOUND_HONBAB("혼밥이 매칭되었습니다. 접속하여 확인해주세요!");
+	MATCHING_FOUND_HONBAB("[세종피어] 밥짝꿍 매칭에 성공했습니다!\n"
+		+ "혼밥탈출에 다시 접속해 밥짝꿍의 정보를 확인해주세요:)");
 
 	private final String value;
 
