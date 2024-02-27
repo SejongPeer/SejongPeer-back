092852c0a15fe26479d4b63c299f62db691a00ba
rong5026 <rong5026@naver.com>
2024-02-27 13:32:49 +0900

    :bug: 버디 매칭 정보요청 응답 Response 수정

--- src/main/java/com/sejong/sejongpeer/domain/buddy/dto/response/PartnerInfoResponse.java
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/dto/response/PartnerInfoResponse.java
@@ -1,9 +1,7 @@
 package com.sejong.sejongpeer.domain.buddy.dto.response;
 
-import com.sejong.sejongpeer.domain.college.entity.CollegeMajor;
-
 public record PartnerInfoResponse(
-	CollegeMajor collegeMajor,
+	String collegeMajor,
 	Integer grade
 ) {
 }
