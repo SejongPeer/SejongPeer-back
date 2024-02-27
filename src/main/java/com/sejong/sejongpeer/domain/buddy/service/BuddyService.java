45a6549de8ee2ca4d964bff69ca657a9cf6414b9
rong5026 <rong5026@naver.com>
2024-02-27 16:26:02 +0900

    :sparkles: 매칭이 Completed 된 후 상대방 정보 요청

--- src/main/java/com/sejong/sejongpeer/domain/buddy/service/BuddyService.java
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/service/BuddyService.java
@@ -8,10 +8,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.sejong.sejongpeer.domain.buddy.constant.LimitTimeConstant;
 import com.sejong.sejongpeer.domain.buddy.dto.request.RegisterRequest;
+import com.sejong.sejongpeer.domain.buddy.dto.response.CompletedPartnerInfoResponse;
 import com.sejong.sejongpeer.domain.buddy.dto.response.MatchingStatusResponse;
 import com.sejong.sejongpeer.domain.buddy.dto.response.PartnerInfoResponse;
 import com.sejong.sejongpeer.domain.buddy.entity.buddy.Buddy;
 import com.sejong.sejongpeer.domain.buddy.entity.buddy.type.BuddyStatus;
+import com.sejong.sejongpeer.domain.buddy.entity.buddymatched.BuddyMatched;
+import com.sejong.sejongpeer.domain.buddy.entity.buddymatched.type.BuddyMatchedStatus;
 import com.sejong.sejongpeer.domain.buddy.repository.BuddyRepository;
 import com.sejong.sejongpeer.domain.member.entity.Member;
 import com.sejong.sejongpeer.domain.member.repository.MemberRepository;
@@ -103,17 +106,54 @@ public class BuddyService {
 
 		Buddy latestBuddy = getLastBuddyByMemberId(memberId)
 			.orElseThrow(() -> new CustomException(ErrorCode.BUDDY_NOT_FOUND));
+		checkBuddyStatus(latestBuddy, BuddyStatus.FOUND_BUDDY);
 
-		if (latestBuddy.getStatus() != BuddyStatus.FOUND_BUDDY) {
-			throw new CustomException(ErrorCode.BUDDY_NOT_MATCHED);
-		}
+		BuddyMatched latestBuddyMatched = buddyMatchingService.getLatestBuddyMatched(latestBuddy);
+		checkBuddyMatchedStatus(latestBuddyMatched, BuddyMatchedStatus.IN_PROGRESS);
 
-		Buddy partner =  buddyMatchingService.findTargetBuddy(latestBuddy);
-
+		Buddy partner =  buddyMatchingService.getOtherBuddyInBuddyMatched(latestBuddyMatched, latestBuddy);
 		Member partnerMember = partner.getMember();
 
-		return (new PartnerInfoResponse(partnerMember.getCollegeMajor().getMajor(), partnerMember.getGrade()));
+		return (new PartnerInfoResponse(
+			partnerMember.getCollegeMajor().getMajor(),
+			partnerMember.getGrade()
+		));
 	}
+
+	public CompletedPartnerInfoResponse getBuddyMatchedPartnerDetails(String memberId) {
+
+		Buddy latestBuddy = getLastBuddyByMemberId(memberId)
+			.orElseThrow(() -> new CustomException(ErrorCode.BUDDY_NOT_FOUND));
+		checkBuddyStatus(latestBuddy, BuddyStatus.MATCHING_COMPLETED);
+
+		BuddyMatched latestBuddyMatched = buddyMatchingService.getLatestBuddyMatched(latestBuddy);
+		checkBuddyMatchedStatus(latestBuddyMatched, BuddyMatchedStatus.MATCHING_COMPLETED);
+
+		Buddy partner =  buddyMatchingService.getOtherBuddyInBuddyMatched(latestBuddyMatched, latestBuddy);
+		Member partnerMember = partner.getMember();
+
+		return (new CompletedPartnerInfoResponse(
+			partnerMember.getCollegeMajor().getMajor(),
+			partnerMember.getGrade(),
+			partnerMember.getName(),
+			partnerMember.getKakaoAccount()
+		));
+	}
+
+	private void checkBuddyStatus(Buddy buddy, BuddyStatus status) {
+		if (buddy.getStatus() != status) {
+			throw new CustomException(ErrorCode.BUDDY_NOT_MATCHED);
+		}
+	}
+
+	private void checkBuddyMatchedStatus(BuddyMatched buddyMatched, BuddyMatchedStatus status) {
+		if (buddyMatched.getStatus() != status) {
+			throw new CustomException(ErrorCode.NOT_IN_PROGRESS);
+		}
+	}
+
+
+
 
 }
 
