d7769a3940d9947c1c9a22cd5f2faa83896be2df
swandevson <shai4956@gmail.com>
2024-02-29 05:10:44 +0900

    :sparkles: 로그인 response 수정

--- src/main/java/com/sejong/sejongpeer/domain/auth/dto/response/SignInResponse.java
+++ src/main/java/com/sejong/sejongpeer/domain/auth/dto/response/SignInResponse.java
@@ -1,3 +1,32 @@
 package com.sejong.sejongpeer.domain.auth.dto.response;
 
-public record SignInResponse(String accessToken, String refreshToken) {}
+import com.sejong.sejongpeer.domain.member.entity.Member;
+import com.sejong.sejongpeer.domain.member.entity.type.Gender;
+
+public record SignInResponse(String accessToken,
+							 String refreshToken,
+							 String kakaoAccount,
+							 String name,
+							 String major,
+							 String minor,
+							 String nickname,
+							 String phoneNumber,
+							 String account,
+							 String studentId,
+							 Gender gender
+) {
+	public static SignInResponse of(String accessToken, String refreshToken, Member member) {
+		return new SignInResponse(
+			accessToken,
+			refreshToken,
+			member.getKakaoAccount(),
+			member.getName(),
+			member.getCollegeMajor().getMajor(),
+			member.getCollegeMinor() != null ? member.getCollegeMinor().getMajor() : null,
+			member.getNickname(),
+			member.getPhoneNumber(),
+			member.getAccount(),
+			member.getStudentId(),
+			member.getGender());
+	}
+}
