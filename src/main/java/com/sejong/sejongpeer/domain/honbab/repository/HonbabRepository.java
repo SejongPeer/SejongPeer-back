3c48657fd7cb2540b633aae76f39a2a89260bcee
Subin <rla0010101@gmail.com>
2024-02-29 08:58:52 +0900

    :sparkles: HonbabRepository memberId로 Honbab 찾는 메소드 추가

--- src/main/java/com/sejong/sejongpeer/domain/honbab/repository/HonbabRepository.java
+++ src/main/java/com/sejong/sejongpeer/domain/honbab/repository/HonbabRepository.java
@@ -1,12 +1,18 @@
 package com.sejong.sejongpeer.domain.honbab.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sejong.sejongpeer.domain.honbab.entity.honbab.Honbab;
 import com.sejong.sejongpeer.domain.honbab.entity.honbab.type.HonbabStatus;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HonbabRepository extends JpaRepository<Honbab, Long> {
 	List<Honbab> findAllByStatus(HonbabStatus status);
+
+	@Query("SELECT b FROM Honbab b WHERE b.member.id = :memberId ORDER BY b.id DESC LIMIT 1")
+	Optional<Honbab> findLastHonbabByMemberId(@Param("memberId") String memberId);
 }
