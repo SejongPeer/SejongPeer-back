b142e3e8cf8acac78de3d146663d13aac2e75617
rong5026 <rong5026@naver.com>
2024-02-28 23:18:09 +0900

    :fire: 혼밥 상대 정보 제공 기능 삭제

--- src/main/java/com/sejong/sejongpeer/domain/honbab/repository/HonbabMatchedRepository.java
+++ src/main/java/com/sejong/sejongpeer/domain/honbab/repository/HonbabMatchedRepository.java
@@ -1,16 +1,10 @@
 package com.sejong.sejongpeer.domain.honbab.repository;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import com.sejong.sejongpeer.domain.honbab.entity.honbab.Honbab;
 import com.sejong.sejongpeer.domain.honbab.entity.honbabmatched.HonbabMatched;
 
 public interface HonbabMatchedRepository extends JpaRepository<HonbabMatched, Long> {
-	@Query("SELECT bm FROM HonbabMatched bm WHERE bm.owner = :owner OR bm.partner = :owner ORDER BY bm.id DESC LIMIT 1")
-	Optional<HonbabMatched> findLatestByOwnerOrPartner(@Param("owner") Honbab owner);
 
 }
