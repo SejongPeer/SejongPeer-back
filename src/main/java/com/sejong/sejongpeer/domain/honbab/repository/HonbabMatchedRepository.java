66d5f7dc5719620dde184140c606eb160614d63e
Subin <rla0010101@gmail.com>
2024-02-29 08:57:27 +0900

    :sparkles: HonbabMatchedRepository owner가 포함된 HonbabMatched 찾는 메소드 추가

--- src/main/java/com/sejong/sejongpeer/domain/honbab/repository/HonbabMatchedRepository.java
+++ src/main/java/com/sejong/sejongpeer/domain/honbab/repository/HonbabMatchedRepository.java
@@ -1,8 +1,16 @@
 package com.sejong.sejongpeer.domain.honbab.repository;
 
+import com.sejong.sejongpeer.domain.honbab.entity.honbab.Honbab;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sejong.sejongpeer.domain.honbab.entity.honbabmatched.HonbabMatched;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface HonbabMatchedRepository extends JpaRepository<HonbabMatched, Long> {
+
+	@Query("SELECT bm FROM HonbabMatched bm WHERE bm.owner = :owner OR bm.partner = :owner ORDER BY bm.id DESC LIMIT 1")
+	Optional<HonbabMatched> findLatestByOwnerOrPartner(@Param("owner") Honbab owner);
 }
