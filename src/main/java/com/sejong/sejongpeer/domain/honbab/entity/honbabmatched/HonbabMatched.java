9d84782d3f37ef7b5d2b3e601a54009ce1cf02b5
rong5026 <rong5026@naver.com>
2024-02-28 19:38:20 +0900

    :bug: 고유Id IENTITY로 값 자동 설정

--- src/main/java/com/sejong/sejongpeer/domain/honbab/entity/honbabmatched/HonbabMatched.java
+++ src/main/java/com/sejong/sejongpeer/domain/honbab/entity/honbabmatched/HonbabMatched.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -23,6 +25,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class HonbabMatched {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@OneToOne(fetch = FetchType.LAZY)
