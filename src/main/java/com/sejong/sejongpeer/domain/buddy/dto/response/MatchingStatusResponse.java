ad1f756dc42ba6fdc4cd8ba4a38253739b8a4686
rong5026 <rong5026@naver.com>
2024-02-28 19:37:01 +0900

    :heavy_plus_sign: 혼밥 에러코드 추가

--- src/main/java/com/sejong/sejongpeer/domain/buddy/dto/response/MatchingStatusResponse.java
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/dto/response/MatchingStatusResponse.java
@@ -1,6 +1,5 @@
 package com.sejong.sejongpeer.domain.buddy.dto.response;
 
-import com.sejong.sejongpeer.domain.buddy.entity.buddy.type.BuddyStatus;
 
-public record MatchingStatusResponse(BuddyStatus status) {
+public record MatchingStatusResponse(String status) {
 }
