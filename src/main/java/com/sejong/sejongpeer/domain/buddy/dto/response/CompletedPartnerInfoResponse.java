63cce3e3c1b4799602415f3003ad93d0b57fdde5
rong5026 <rong5026@naver.com>
2024-02-29 09:47:38 +0900

    :art: 버디 상대방 정보 응답 from 추가

--- src/main/java/com/sejong/sejongpeer/domain/buddy/dto/response/CompletedPartnerInfoResponse.java
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/dto/response/CompletedPartnerInfoResponse.java
@@ -1,9 +1,19 @@
 package com.sejong.sejongpeer.domain.buddy.dto.response;
 
+import com.sejong.sejongpeer.domain.member.entity.Member;
+
 public record CompletedPartnerInfoResponse(
 	String collegeMajor,
 	Integer grade,
 	String name,
 	String kakaoAccount
 ) {
+	public static CompletedPartnerInfoResponse from(Member member) {
+		return new CompletedPartnerInfoResponse(
+			member.getCollegeMajor().getMajor(),
+			member.getGrade(),
+			member.getName(),
+			member.getKakaoAccount()
+		);
+	}
 }
--- src/main/java/com/sejong/sejongpeer/domain/buddy/dto/response/PartnerInfoResponse.java
+++
@@ -1,7 +0,0 @@
-package com.sejong.sejongpeer.domain.buddy.dto.response;
-
-public record PartnerInfoResponse(
-	String collegeMajor,
-	Integer grade
-) {
-}
