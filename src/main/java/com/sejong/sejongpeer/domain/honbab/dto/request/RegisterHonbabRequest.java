7db23182f98ba6d4af7a69cf52474b7b999deb49
Subin <rla0010101@gmail.com>
2024-03-01 15:49:29 +0900

    :art: 대기 시간 받지 않고 정해진 15분이 초과되면 혼밥 매칭 스케쥴링 대기열에서 제외되도록 수정

--- src/main/java/com/sejong/sejongpeer/domain/honbab/dto/request/RegisterHonbabRequest.java
+++ src/main/java/com/sejong/sejongpeer/domain/honbab/dto/request/RegisterHonbabRequest.java
@@ -9,6 +9,5 @@ import jakarta.validation.constraints.NotNull;
 
 public record RegisterHonbabRequest(
 	@NotNull(message = "선호 성별을 선택해주세요.") GenderOption genderOption,
-	@NotNull(message = "원하는 메뉴 카테고리를 선택해주세요.") MenuCategoryOption menuCategoryOption,
-	@NotNull(message = "매칭 대기 시간을 선택해주세요.") Duration waitTime
+	@NotNull(message = "원하는 메뉴 카테고리를 선택해주세요.") MenuCategoryOption menuCategoryOption
 ) {}
