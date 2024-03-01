85e346d330ec1407e0e737a7e67132c8016f5e1a
Subin <rla0010101@gmail.com>
2024-03-01 10:16:59 +0900

    :sparkles: RegisterHonbabRequest 대기 시간 Duration 형식으로 추가

--- src/main/java/com/sejong/sejongpeer/domain/honbab/dto/request/RegisterHonbabRequest.java
+++ src/main/java/com/sejong/sejongpeer/domain/honbab/dto/request/RegisterHonbabRequest.java
@@ -1,5 +1,7 @@
 package com.sejong.sejongpeer.domain.honbab.dto.request;
 
+import java.time.Duration;
+
 import com.sejong.sejongpeer.domain.honbab.entity.honbab.type.GenderOption;
 import com.sejong.sejongpeer.domain.honbab.entity.honbab.type.MenuCategoryOption;
 
@@ -7,5 +9,6 @@ import jakarta.validation.constraints.NotNull;
 
 public record RegisterHonbabRequest(
 	@NotNull(message = "선호 성별을 선택해주세요.") GenderOption genderOption,
-	@NotNull(message = "원하는 메뉴 카테고리를 선택해주세요.") MenuCategoryOption menuCategoryOption
+	@NotNull(message = "원하는 메뉴 카테고리를 선택해주세요.") MenuCategoryOption menuCategoryOption,
+	@NotNull(message = "매칭 대기 시간을 선택해주세요.") Duration waitTime
 ) {}
