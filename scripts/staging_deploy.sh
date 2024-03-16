abcc164082da3559695275ab94e5aa0550c19c55
rong5026 <rong5026@naver.com>
2024-03-16 18:24:30 +0900

    :zap: test

--- scripts/staging_deploy.sh
+++ scripts/staging_deploy.sh
@@ -9,7 +9,7 @@ CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ] #2
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다!."
 else
   echo "> kill -15 $CURRENT_PID"
   sudo kill -15 $CURRENT_PID
