d91bab2b00a6ccd873b292e81271bea4bafa6527
Subin <rla0010101@gmail.com>
2024-03-21 15:50:33 +0900

    :recycle: 자동 거절 처리 스케쥴러 24시간 상수 처리로 리팩토링

--- src/main/java/com/sejong/sejongpeer/domain/buddy/scheduler/MatchingScheduler.java
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/scheduler/MatchingScheduler.java
@@ -4,11 +4,9 @@ import com.sejong.sejongpeer.domain.buddy.entity.buddy.Buddy;
 import com.sejong.sejongpeer.domain.buddy.entity.buddy.type.BuddyStatus;
 import com.sejong.sejongpeer.domain.buddy.entity.buddymatched.BuddyMatched;
 import com.sejong.sejongpeer.domain.buddy.entity.buddymatched.type.BuddyMatchedStatus;
-import com.sejong.sejongpeer.domain.buddy.repository.BuddyMatchedRepository;
 import com.sejong.sejongpeer.domain.buddy.repository.BuddyRepository;
 import com.sejong.sejongpeer.domain.buddy.service.BuddyMatchingService;
 import com.sejong.sejongpeer.domain.buddy.service.MatchingService;
-import com.sejong.sejongpeer.infra.sms.service.SmsService;
 import com.sejong.sejongpeer.infra.sms.service.SmsText;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,6 +19,8 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class MatchingScheduler {
+	private static final int NO_RESPONSE_HOUR_LIMIT = 24;
+
 	private final BuddyRepository buddyRepository;
     private final MatchingService matchingService;
 	private final BuddyMatchingService buddyMatchingService;
@@ -40,7 +40,7 @@ public class MatchingScheduler {
 			LocalDateTime updatedTime = NoResposeBuddy.getUpdatedAt();
 			long hoursElapsed = ChronoUnit.HOURS.between(updatedTime, currentTime);
 
-			if (hoursElapsed >= 24) {
+			if (hoursElapsed >= NO_RESPONSE_HOUR_LIMIT) {
 				NoResposeBuddy.changeStatus(BuddyStatus.REJECT);
 				buddyMatchingService.sendMatchingFailurePenaltyMessage(NoResposeBuddy, SmsText.MATCHING_AUTO_FAILED_REJECT);
 
