5a639aae1e5f8f991775341b37e83662762897a3
swandevson <shai4956@gmail.com>
2024-03-27 18:53:14 +0900

    :recycle: SecurityContextHolder에서 MemberId 추출 부분 메소드로 분리

--- src/main/java/com/sejong/sejongpeer/domain/buddy/api/BuddyController.java
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/api/BuddyController.java
@@ -1,23 +1,25 @@
 package com.sejong.sejongpeer.domain.buddy.api;
 
-import com.sejong.sejongpeer.domain.buddy.dto.request.RegisterRequest;
-import com.sejong.sejongpeer.domain.buddy.dto.response.CompletedPartnerInfoResponse;
-import com.sejong.sejongpeer.domain.buddy.dto.response.MatchingStatusResponse;
-import com.sejong.sejongpeer.domain.buddy.dto.response.MatchingPartnerInfoResponse;
-import com.sejong.sejongpeer.domain.buddy.dto.response.ActiveCustomersCountResponse;
-import com.sejong.sejongpeer.domain.buddy.service.BuddyService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.sejong.sejongpeer.domain.buddy.dto.request.RegisterRequest;
+import com.sejong.sejongpeer.domain.buddy.dto.response.ActiveCustomersCountResponse;
+import com.sejong.sejongpeer.domain.buddy.dto.response.CompletedPartnerInfoResponse;
+import com.sejong.sejongpeer.domain.buddy.dto.response.MatchingPartnerInfoResponse;
+import com.sejong.sejongpeer.domain.buddy.dto.response.MatchingStatusResponse;
+import com.sejong.sejongpeer.domain.buddy.service.BuddyService;
+import com.sejong.sejongpeer.security.util.SecurityContextUtil;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @Tag(name = "4. [버디]", description = "세종버디 API")
 @RestController
@@ -29,40 +31,35 @@ public class BuddyController {
 	@Operation(summary = "버디등록", description = "유저가 버디에 등록")
 	@PostMapping("/register")
 	public void registerBuddy(@Valid @RequestBody RegisterRequest request) {
-		String memberId =
-			(String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String memberId = SecurityContextUtil.extractMemberId();
 		buddyService.registerBuddy(request, memberId);
 	}
 
 	@Operation(summary = "버디 중도 취소", description = "매칭 전 버디 취소")
 	@GetMapping("/cancel")
 	public void cancelBuddy() {
-		String memberId =
-			(String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String memberId = SecurityContextUtil.extractMemberId();
 		buddyService.cancelBuddy(memberId);
 	}
 
 	@Operation(summary = "버디 매칭 상태 체크", description = "유저의 가장 최근 버디 상태값 리턴")
 	@GetMapping("/check-matching-status")
 	public MatchingStatusResponse getMatchingStatus() {
-		String memberId =
-			(String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String memberId = SecurityContextUtil.extractMemberId();
 		return buddyService.getBuddyMatchingStatuAndCount(memberId);
 	}
 
 	@Operation(summary = "버디 수락/거절 창에서의 상대방 정보 요청", description = "매칭 후 상대방의 학과, 학년 정보 리턴")
 	@GetMapping("/partner/details")
 	public MatchingPartnerInfoResponse getPartnerDetails() {
-		String memberId =
-			(String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String memberId = SecurityContextUtil.extractMemberId();
 		return buddyService.getBuddyMatchingPartnerDetails(memberId);
 	}
 
 	@Operation(summary = "매칭 수락 완료 후 상대방 정보요청", description = "이름, 학년, 학과, 카카오톡 정보 리턴")
 	@GetMapping("/matched/partner/details")
 	public List<CompletedPartnerInfoResponse> getBuddyMatchedPartnerDetails() {
-		String memberId =
-			(String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String memberId = SecurityContextUtil.extractMemberId();
 		return buddyService.getBuddyMatchedPartnerDetails(memberId);
 	}
 
--- src/main/java/com/sejong/sejongpeer/domain/buddy/api/BuddyMatchingController.java
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/api/BuddyMatchingController.java
@@ -1,16 +1,18 @@
 package com.sejong.sejongpeer.domain.buddy.api;
 
-import com.sejong.sejongpeer.domain.buddy.dto.request.MatchingResultRequest;
-import com.sejong.sejongpeer.domain.buddy.service.BuddyMatchingService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import com.sejong.sejongpeer.domain.buddy.dto.request.MatchingResultRequest;
+import com.sejong.sejongpeer.domain.buddy.service.BuddyMatchingService;
+import com.sejong.sejongpeer.security.util.SecurityContextUtil;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @Tag(name = "5. [버디 매칭]", description = "세종버디 API")
 @RestController
@@ -22,7 +24,7 @@ public class BuddyMatchingController {
 	@Operation(summary = "버디 매칭 상태 업데이트", description = "버디 매칭 상태 관리")
 	@PostMapping("/status")
 	public void checkBuddyMatching(@Valid @RequestBody MatchingResultRequest request) {
-		String memberId = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String memberId = SecurityContextUtil.extractMemberId();
 		buddyMatchingService.updateBuddyMatchingStatus(memberId, request);
 	}
 }
