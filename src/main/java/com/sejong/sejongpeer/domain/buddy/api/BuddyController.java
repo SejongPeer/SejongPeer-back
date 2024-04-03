97dc96834874f572ebe026e391be41328bb4fd63
swandevson <shai4956@gmail.com>
2024-04-03 11:05:26 +0900

    :bug: 오타 수정

--- src/main/java/com/sejong/sejongpeer/domain/buddy/api/BuddyController.java
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/api/BuddyController.java
@@ -1,21 +1,6 @@
 package com.sejong.sejongpeer.domain.buddy.api;
 
-import com.sejong.sejongpeer.domain.buddy.dto.request.MatchingResultRequest;
-import com.sejong.sejongpeer.domain.buddy.dto.request.RegisterRequest;
-import com.sejong.sejongpeer.domain.buddy.dto.response.ActiveCustomersCountResponse;
-import com.sejong.sejongpeer.domain.buddy.dto.response.CompletedPartnerInfoResponse;
-import com.sejong.sejongpeer.domain.buddy.dto.response.MatchingPartnerInfoResponse;
-import com.sejong.sejongpeer.domain.buddy.dto.response.ActiveCustomersCountResponse;
-import com.sejong.sejongpeer.domain.buddy.service.BuddyMatchingService;
-import com.sejong.sejongpeer.domain.buddy.dto.response.MatchingStatusResponse;
-import com.sejong.sejongpeer.domain.buddy.service.BuddyService;
-
-import com.sejong.sejongpeer.security.util.SecurityContextUtil;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,7 +9,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.sejong.sejongpeer.domain.buddy.dto.request.MatchingResultRequest;
+import com.sejong.sejongpeer.domain.buddy.dto.request.RegisterRequest;
+import com.sejong.sejongpeer.domain.buddy.dto.response.ActiveCustomersCountResponse;
+import com.sejong.sejongpeer.domain.buddy.dto.response.CompletedPartnerInfoResponse;
+import com.sejong.sejongpeer.domain.buddy.dto.response.MatchingPartnerInfoResponse;
+import com.sejong.sejongpeer.domain.buddy.dto.response.MatchingStatusResponse;
+import com.sejong.sejongpeer.domain.buddy.service.BuddyMatchingService;
+import com.sejong.sejongpeer.domain.buddy.service.BuddyService;
+import com.sejong.sejongpeer.security.util.SecurityContextUtil;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @Tag(name = "4. [버디]", description = "세종버디 API")
 @RestController
@@ -52,7 +50,7 @@ public class BuddyController {
 	@GetMapping("/check-matching-status")
 	public MatchingStatusResponse getMatchingStatus() {
 		String memberId = SecurityContextUtil.extractMemberId();
-		return buddyService.getBuddyMatchingStatuAndCount(memberId);
+		return buddyService.getBuddyMatchingStatusAndCount(memberId);
 	}
 
 	@Operation(summary = "버디 수락/거절 창에서의 상대방 정보 요청", description = "매칭 후 상대방의 학과, 학년 정보 리턴")
@@ -79,7 +77,7 @@ public class BuddyController {
 	@Operation(summary = "버디 매칭 이후 수락/거절 선택", description = "버디 최종 매칭 상태 등록")
 	@PostMapping("/matching/status")
 	public void processBuddyMatchingDecision(@Valid @RequestBody MatchingResultRequest request) {
-		String memberId = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String memberId = (String)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		buddyMatchingService.updateBuddyMatchedStatus(memberId, request);
 	}
 }
