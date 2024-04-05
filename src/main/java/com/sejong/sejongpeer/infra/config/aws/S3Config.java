170496076f1974644011721fb0c834b11ba28bc1
ybchar <uiurihappy@sju.ac.kr>
2024-04-05 19:23:33 +0900

    fix: s3Config credentials

--- src/main/java/com/sejong/sejongpeer/infra/config/aws/S3Config.java
+++ src/main/java/com/sejong/sejongpeer/infra/config/aws/S3Config.java
@@ -2,8 +2,8 @@ package com.sejong.sejongpeer.infra.config.aws;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
@@ -19,13 +19,16 @@ public class S3Config {
 	private final S3Properties s3Properties;
 
 	@Bean
-	public AmazonS3 amazonS3() {
-		AWSCredentials credentials =
-			new BasicAWSCredentials(
-				s3Properties.accessKey(), s3Properties.secretKey());
+	@Primary
+	public BasicAWSCredentials awsCredentialsProvider() {
+		return new BasicAWSCredentials(s3Properties.accessKey(),
+			s3Properties.secretKey());
+	}
 
+	@Bean
+	public AmazonS3 amazonS3() {
 		return AmazonS3ClientBuilder.standard()
-			.withCredentials(new AWSStaticCredentialsProvider(credentials))
+			.withCredentials(new AWSStaticCredentialsProvider(awsCredentialsProvider()))
 			.withRegion(s3Properties.region())
 			.build();
 	}
