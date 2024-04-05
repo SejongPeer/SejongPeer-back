c862b29bdd00007e10d4ad7db6cc042188d426cf
Subin <rla0010101@gmail.com>
2024-04-05 18:51:09 +0900

    :recycle: 파일명 접두어 College 통일

---
+++ src/main/java/com/sejong/sejongpeer/domain/college/dto/CollegeLessonProfResponse.java
@@ -0,0 +1,4 @@
+package com.sejong.sejongpeer.domain.college.dto;
+
+public record CollegeLessonProfResponse(String lesson, String prof) {
+}
--- src/main/java/com/sejong/sejongpeer/domain/college/dto/LessonProfResponse.java
+++
@@ -1,4 +0,0 @@
-package com.sejong.sejongpeer.domain.college.dto;
-
-public record LessonProfResponse(String lesson, String prof) {
-}
