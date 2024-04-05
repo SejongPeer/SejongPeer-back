c2adfd9301d2dcc73da84222f55654b3a3a023cb
Subin <rla0010101@gmail.com>
2024-04-05 20:13:21 +0900

    :art: controller swagger annotation 추가

--- src/main/java/com/sejong/sejongpeer/domain/college/api/CollegeLessonProfController.java
+++ src/main/java/com/sejong/sejongpeer/domain/college/api/CollegeLessonProfController.java
@@ -2,6 +2,8 @@ package com.sejong.sejongpeer.domain.college.api;
 
 import com.sejong.sejongpeer.domain.college.dto.CollegeLessonProfResponse;
 import com.sejong.sejongpeer.domain.college.service.CollegeLessonProfService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "3. [스터디]", description = "스터디 관련 API입니다.")
 @RestController
 @RequestMapping("/api/v1/colleges")
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class CollegeLessonProfController {
 
 	private final CollegeLessonProfService lessonProfService;
 
+	@Operation(summary = "수업 정보 조회", description = "스터디 게시글 작성에 필요한 단과대 별 개설된 수업 정보를 반환합니다.")
 	@GetMapping("/lessons-info")
 	public List<CollegeLessonProfResponse> getLessonInfoByColleage(
 		@RequestParam(name = "college") String college) {
