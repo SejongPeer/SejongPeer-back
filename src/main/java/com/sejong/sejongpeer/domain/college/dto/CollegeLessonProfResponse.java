1a6ddd63df29fc66a276036b8259a313509b8588
Subin <rla0010101@gmail.com>
2024-04-05 19:50:58 +0900

    :art: CollegeLessonProfResponse 팩토리 메소드 적용

--- src/main/java/com/sejong/sejongpeer/domain/college/dto/CollegeLessonProfResponse.java
+++ src/main/java/com/sejong/sejongpeer/domain/college/dto/CollegeLessonProfResponse.java
@@ -1,4 +1,12 @@
 package com.sejong.sejongpeer.domain.college.dto;
 
+import com.sejong.sejongpeer.domain.college.entity.CollegeLessonProf;
+
 public record CollegeLessonProfResponse(String lesson, String prof) {
+	public static CollegeLessonProfResponse from(CollegeLessonProf collegeLessonProf) {
+		return new CollegeLessonProfResponse(
+			collegeLessonProf.getLesson(),
+			collegeLessonProf.getProf()
+		);
+	}
 }
