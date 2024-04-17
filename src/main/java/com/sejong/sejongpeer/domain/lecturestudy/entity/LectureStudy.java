d12fb490d89f706ffd8db159e91c42f8563cdf6e
ybchar <uiurihappy@sju.ac.kr>
2024-04-17 21:55:44 +0900

    fix: conflict 해결

--- src/main/java/com/sejong/sejongpeer/domain/lecturestudy/entity/LectureStudy.java
+++ src/main/java/com/sejong/sejongpeer/domain/lecturestudy/entity/LectureStudy.java
@@ -1,13 +1,12 @@
 package com.sejong.sejongpeer.domain.lecturestudy.entity;
 
-import com.sejong.sejongpeer.domain.common.BaseEntity;
+import com.sejong.sejongpeer.domain.common.BaseAuditEntity;
 import com.sejong.sejongpeer.domain.study.entity.Study;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
@@ -18,16 +17,14 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class LectureStudy extends BaseEntity {
+public class LectureStudy extends BaseAuditEntity {
 	@Id
 	private Long studyId;
 
 	@MapsId
 	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-	@JoinColumn(name = "study_id")
-	private Study lectureStudy;
+	private Study study;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "lecture_id")
 	private Lecture lecture;
 }
