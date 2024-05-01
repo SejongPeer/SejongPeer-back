f916b311c6bd32fa798f67b92e87f56ffee51714
Subin <rla0010101@gmail.com>
2024-05-01 15:19:09 +0900

    :recycle: lectureService 데이터 수정 작업 고려하여 stream 분할

---
+++ src/main/java/com/sejong/sejongpeer/domain/lecturestudy/service/LectureService.java
@@ -0,0 +1,29 @@
+package com.sejong.sejongpeer.domain.lecturestudy.service;
+
+import com.sejong.sejongpeer.domain.lecturestudy.dto.CollegeLessonProfessorResponse;
+import com.sejong.sejongpeer.domain.lecturestudy.entity.Lecture;
+import com.sejong.sejongpeer.domain.lecturestudy.repository.LectureRepository;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class LectureService {
+
+	private final LectureRepository lectureRepository;
+
+	@Transactional(readOnly = true)
+	public List<CollegeLessonProfessorResponse> getLessonInfoByColleage(String college) {
+		List<Lecture> lectures = lectureRepository.findAllByCollege(college);
+		List<CollegeLessonProfessorResponse> lessonInfo = lectures.stream()
+			.map(CollegeLessonProfessorResponse::from)
+			.collect(Collectors.toList());
+		return Collections.unmodifiableList(lessonInfo);
+	}
+}
