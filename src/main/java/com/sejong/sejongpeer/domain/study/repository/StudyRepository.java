c13ca509f60a8a46ef2c27cb4956e8510eecb403
Subin <rla0010101@gmail.com>
2024-05-15 19:00:37 +0900

    :sparkles: StudyType에 따른 스터디 목록 조회를 위한 StudyRepository 메소드 추가

--- src/main/java/com/sejong/sejongpeer/domain/study/repository/StudyRepository.java
+++ src/main/java/com/sejong/sejongpeer/domain/study/repository/StudyRepository.java
@@ -1,6 +1,12 @@
 package com.sejong.sejongpeer.domain.study.repository;
 
 import com.sejong.sejongpeer.domain.study.entity.Study;
+import com.sejong.sejongpeer.domain.study.entity.type.StudyType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudyRepository extends JpaRepository<Study, Long>, StudyRepositoryCustom {}
+import java.util.List;
+
+public interface StudyRepository extends JpaRepository<Study, Long>, StudyRepositoryCustom {
+
+	List<Study> findByType(StudyType studyType);
+}
