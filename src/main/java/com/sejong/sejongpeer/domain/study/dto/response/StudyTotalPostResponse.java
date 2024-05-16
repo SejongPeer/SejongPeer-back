6cff501386b6de80eb900a54148be699982ae59d
Subin <rla0010101@gmail.com>
2024-05-16 12:40:26 +0900

    :art: StudyTotalPostResponse 반환 값 추가

--- src/main/java/com/sejong/sejongpeer/domain/study/dto/response/StudyTotalPostResponse.java
+++ src/main/java/com/sejong/sejongpeer/domain/study/dto/response/StudyTotalPostResponse.java
@@ -3,6 +3,7 @@ package com.sejong.sejongpeer.domain.study.dto.response;
 import java.util.List;
 
 public record StudyTotalPostResponse(
+	Long id,
 	String title,
 	String createdAt,
 	Boolean isImage,
