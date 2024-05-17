fc553f81af23cfae61201765e565a3d90c5d885b
Subin <rla0010101@gmail.com>
2024-05-17 19:14:57 +0900

    :recycle: 학교 수업 스터디, 수업 외 활동 스터디 목록 조회 로직 리펙토링

--- src/main/java/com/sejong/sejongpeer/domain/study/dto/response/StudyTotalPostResponse.java
+++ src/main/java/com/sejong/sejongpeer/domain/study/dto/response/StudyTotalPostResponse.java
@@ -1,11 +1,34 @@
 package com.sejong.sejongpeer.domain.study.dto.response;
 
+import com.sejong.sejongpeer.domain.study.entity.Study;
+
 public record StudyTotalPostResponse(
 	Long id,
 	String title,
 	String createdAt,
-	Boolean isImage,
-	String postTag
-
+	boolean hasImage,
+	String categoryName
 ) {
+	public static StudyTotalPostResponse fromLectureStudy(Study study, String lectureName) {
+		boolean hasImage = study.getImageUrl() != null;
+		return new StudyTotalPostResponse(
+			study.getId(),
+			study.getTitle(),
+			study.getCreatedAt().toString().substring(0, 10),
+			hasImage,
+			lectureName
+		);
+	}
+
+	public static StudyTotalPostResponse fromExternalActivityStudy(Study study, String activityCategoryName) {
+		boolean hasImage = study.getImageUrl() != null;
+		return new StudyTotalPostResponse(
+			study.getId(),
+			study.getTitle(),
+			study.getCreatedAt().toString().substring(0, 10),
+			hasImage,
+			activityCategoryName
+		);
+	}
 }
+
