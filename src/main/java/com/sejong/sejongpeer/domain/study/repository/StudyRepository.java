2a70e687ec914b402e47c11b45bddbe607f458c1
Subin <rla0010101@gmail.com>
2024-05-18 15:36:04 +0900

    :sparkles: StudyRepository Slice 반환 메소드 추가

--- src/main/java/com/sejong/sejongpeer/domain/study/repository/StudyRepository.java
+++ src/main/java/com/sejong/sejongpeer/domain/study/repository/StudyRepository.java
@@ -2,11 +2,16 @@ package com.sejong.sejongpeer.domain.study.repository;
 
 import com.sejong.sejongpeer.domain.study.entity.Study;
 import com.sejong.sejongpeer.domain.study.entity.type.StudyType;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StudyRepository extends JpaRepository<Study, Long>, StudyRepositoryCustom {
 
 	List<Study> findByType(StudyType studyType);
+
+	Slice<Study> findByTypeAndCreatedAtAfter(StudyType studyType, LocalDateTime createdAt, Pageable pageable);
 }
