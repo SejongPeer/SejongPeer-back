64cc27b41ef40951feb8937f50c295c501a025d9
swandevson <shai4956@gmail.com>
2024-05-20 02:53:24 +0900

    :art: codestyle 적용

--- src/main/java/com/sejong/sejongpeer/domain/study/repository/StudyRepositoryImpl.java
+++ src/main/java/com/sejong/sejongpeer/domain/study/repository/StudyRepositoryImpl.java
@@ -5,8 +5,11 @@ import static com.sejong.sejongpeer.domain.study.entity.QStudy.*;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sejong.sejongpeer.domain.study.entity.Study;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
@@ -15,36 +18,36 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class StudyRepositoryImpl implements StudyRepositoryCustom {
-    private final JPAQueryFactory jpaQueryFactory;
+	private final JPAQueryFactory jpaQueryFactory;
 
-    @Override
-    public Slice<Study> findStudySlice(int size, Long lastId) {
-        List<Study> studies =
-                jpaQueryFactory
-                        .selectFrom(study)
-                        .where(ltStudyId(lastId))
-                        .orderBy(study.id.desc())
-                        .limit((long) size + 1)
-                        .fetch();
-        return checkLastPage(size, studies);
-    }
+	@Override
+	public Slice<Study> findStudySlice(int size, Long lastId) {
+		List<Study> studies =
+			jpaQueryFactory
+				.selectFrom(study)
+				.where(ltStudyId(lastId))
+				.orderBy(study.id.desc())
+				.limit((long)size + 1)
+				.fetch();
+		return checkLastPage(size, studies);
+	}
 
-    private BooleanExpression ltStudyId(Long lastId) {
-        if (lastId == null) {
-            return null;
-        }
-        return study.id.lt(lastId);
-    }
+	private BooleanExpression ltStudyId(Long lastId) {
+		if (lastId == null) {
+			return null;
+		}
+		return study.id.lt(lastId);
+	}
 
-    private Slice<Study> checkLastPage(int size, List<Study> result) {
-        boolean hasNext = false;
+	private Slice<Study> checkLastPage(int size, List<Study> result) {
+		boolean hasNext = false;
 
-        // 조회한 결과 개수가 요청한 페이지 사이즈보다 크면 뒤에 더 있음, next = true
-        if (result.size() > size) {
-            hasNext = true;
-            result.remove(size);
-        }
-        Pageable pageable = Pageable.unpaged();
-        return new SliceImpl<>(result, pageable, hasNext);
-    }
+		// 조회한 결과 개수가 요청한 페이지 사이즈보다 크면 뒤에 더 있음, next = true
+		if (result.size() > size) {
+			hasNext = true;
+			result.remove(size);
+		}
+		Pageable pageable = Pageable.unpaged();
+		return new SliceImpl<>(result, pageable, hasNext);
+	}
 }
