889d31242349f76da0433aa2065906ebacf82bf2
Subin <rla0010101@gmail.com>
2024-05-24 15:49:22 +0900

    :recycle: findBy 메소드 파라미터 id에서 프록시 객체 전달로 수정

--- src/main/java/com/sejong/sejongpeer/domain/externalactivitystudy/repository/ExternalActivityStudyRepository.java
+++ src/main/java/com/sejong/sejongpeer/domain/externalactivitystudy/repository/ExternalActivityStudyRepository.java
@@ -1,11 +1,12 @@
 package com.sejong.sejongpeer.domain.externalactivitystudy.repository;
 
 import com.sejong.sejongpeer.domain.externalactivitystudy.entity.ExternalActivityStudy;
+import com.sejong.sejongpeer.domain.study.entity.Study;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ExternalActivityStudyRepository extends JpaRepository<ExternalActivityStudy, Long> {
 
-	Optional<ExternalActivityStudy> findByStudyId(Long studyId);
+	Optional<ExternalActivityStudy> findByStudy(Study study);
 }
