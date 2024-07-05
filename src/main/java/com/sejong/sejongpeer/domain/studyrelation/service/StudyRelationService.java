47f7a5281849bc4a7b1d1b3dec4ee1dc86119a32
Subin <rla0010101@gmail.com>
2024-07-05 12:13:43 +0900

    :sparkles: #205 스터디 지원 기능 구현

--- src/main/java/com/sejong/sejongpeer/domain/studyrelation/service/StudyRelationService.java
+++ src/main/java/com/sejong/sejongpeer/domain/studyrelation/service/StudyRelationService.java
@@ -1,5 +1,12 @@
 package com.sejong.sejongpeer.domain.studyrelation.service;
 
+import com.sejong.sejongpeer.domain.study.entity.Study;
+import com.sejong.sejongpeer.domain.study.repository.StudyRepository;
+import com.sejong.sejongpeer.domain.studyrelation.dto.request.StudyApplyRequest;
+import com.sejong.sejongpeer.domain.studyrelation.entity.StudyRelation;
+import com.sejong.sejongpeer.global.error.exception.CustomException;
+import com.sejong.sejongpeer.global.error.exception.ErrorCode;
+import com.sejong.sejongpeer.global.util.MemberUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +20,17 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 public class StudyRelationService {
 
+	private final StudyRepository studyRepository;
 	private final StudyRelationRepository studyRelationRepository;
+	private final MemberUtil memberUtil;
 
-	public StudyRelationCreateResponse createStudyRelation(final Long studyId) {
-		return null;
+	public StudyRelationCreateResponse applyStudy(StudyApplyRequest studyApplyRequest) {
+		Study study = studyRepository.findById(studyApplyRequest.studyId())
+			.orElseThrow(() -> new CustomException(ErrorCode.STUDY_NOT_FOUND));
+
+		StudyRelation studyapplication = StudyRelation.createStudyRelations(memberUtil.getCurrentMember(),study);
+		studyRelationRepository.save(studyapplication);
+
+		return StudyRelationCreateResponse.from(studyapplication);
 	}
 }
