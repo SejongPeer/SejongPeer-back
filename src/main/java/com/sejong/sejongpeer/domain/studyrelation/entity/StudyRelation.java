bcb933d2bb90aa257e7741f32b0905596d4fedc8
Subin <rla0010101@gmail.com>
2024-07-05 13:07:12 +0900

    :sparkles: #205 StudyRelation table member_id, study_id 제약조건 추가

--- src/main/java/com/sejong/sejongpeer/domain/studyrelation/entity/StudyRelation.java
+++ src/main/java/com/sejong/sejongpeer/domain/studyrelation/entity/StudyRelation.java
@@ -1,5 +1,6 @@
 package com.sejong.sejongpeer.domain.studyrelation.entity;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.Comment;
 
 import com.sejong.sejongpeer.domain.common.BaseAuditEntity;
@@ -7,16 +8,6 @@ import com.sejong.sejongpeer.domain.member.entity.Member;
 import com.sejong.sejongpeer.domain.study.entity.Study;
 import com.sejong.sejongpeer.domain.studyrelation.entity.type.StudyMatchingStatus;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,6 +16,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "study_relation", uniqueConstraints = {
+	@jakarta.persistence.UniqueConstraint(columnNames = {"member_id", "study_id"})
+})
 public class StudyRelation extends BaseAuditEntity {
 
 	@Id
