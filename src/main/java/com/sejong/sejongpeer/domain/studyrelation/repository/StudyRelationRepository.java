6472f8c854c8b56b47989745f92b9c39457bcf0a
Subin <rla0010101@gmail.com>
2024-07-05 14:11:23 +0900

    :sparkles: #204 게시글 상세 조회 게시글 별 지원자 수 반환 추가

--- src/main/java/com/sejong/sejongpeer/domain/studyrelation/repository/StudyRelationRepository.java
+++ src/main/java/com/sejong/sejongpeer/domain/studyrelation/repository/StudyRelationRepository.java
@@ -2,6 +2,7 @@ package com.sejong.sejongpeer.domain.studyrelation.repository;
 
 import com.sejong.sejongpeer.domain.member.entity.Member;
 import com.sejong.sejongpeer.domain.study.entity.Study;
+import com.sejong.sejongpeer.domain.studyrelation.entity.type.StudyMatchingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sejong.sejongpeer.domain.studyrelation.entity.StudyRelation;
@@ -12,4 +13,6 @@ public interface StudyRelationRepository
 	extends JpaRepository<StudyRelation, String>, StudyRelationRepositoryCustom {
 
 	List<StudyRelation> findByMemberAndStudy(Member member, Study study);
+
+	Long countByStudyAndStatus(Study study, StudyMatchingStatus status);
 }
