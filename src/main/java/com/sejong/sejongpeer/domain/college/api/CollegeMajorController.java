5f7f75c0368b89ac88c6deb11fb5ae980d6d8dcc
Subin <rla0010101@gmail.com>
2024-07-10 18:49:09 +0900

    :bug: #218 포스트맨 아니어도 프론트에서 access, refresh token으로 스웨거 상에서 테스트 가능하도록 수정 및 스웨거 태그 넘버링 수정

--- src/main/java/com/sejong/sejongpeer/domain/college/api/CollegeMajorController.java
+++ src/main/java/com/sejong/sejongpeer/domain/college/api/CollegeMajorController.java
@@ -3,12 +3,15 @@ package com.sejong.sejongpeer.domain.college.api;
 import com.sejong.sejongpeer.domain.college.dto.CollegeMajorResponse;
 import com.sejong.sejongpeer.domain.college.service.CollegeMajorService;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "0. [학교]", description = "세종대 단과대 별 전공 정보 API")
 @RestController
 @RequestMapping("/api/v1/colleges")
 @RequiredArgsConstructor
