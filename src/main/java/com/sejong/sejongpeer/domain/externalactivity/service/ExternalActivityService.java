022eba237a4a2685de0c4ca6206d3fb22fdcdc25
Subin <rla0010101@gmail.com>
2024-07-10 16:36:39 +0900

    :sparkles: #215 교외 활동 카테고리 조회 서비스

---
+++ src/main/java/com/sejong/sejongpeer/domain/externalactivity/service/ExternalActivityService.java
@@ -0,0 +1,28 @@
+package com.sejong.sejongpeer.domain.externalactivity.service;
+
+import com.sejong.sejongpeer.domain.externalactivity.dto.ExternalActivityCategoryResponse;
+import com.sejong.sejongpeer.domain.externalactivity.entity.ExternalActivity;
+import com.sejong.sejongpeer.domain.externalactivity.repository.ExternalActivityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ExternalActivityService {
+
+	private final ExternalActivityRepository externalActivityRepository;
+
+	@Transactional(readOnly = true)
+	public List<ExternalActivityCategoryResponse> getAllExternalActivityCategories() {
+		List<ExternalActivity> externalActivityList = externalActivityRepository.findAll();
+
+		return externalActivityList.stream()
+			.map(ExternalActivityCategoryResponse::from)
+			.collect(Collectors.toUnmodifiableList());
+	}
+}
