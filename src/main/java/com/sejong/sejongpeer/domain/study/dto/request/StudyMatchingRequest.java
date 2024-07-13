05bd0e9e6171b27394c8fe5047dcc9e9a29eb317
Subin <rla0010101@gmail.com>
2024-07-13 21:39:16 +0900

    :sparkles: #231 스터디 수락/거절 request dto

---
+++ src/main/java/com/sejong/sejongpeer/domain/study/dto/request/StudyMatchingRequest.java
@@ -0,0 +1,10 @@
+package com.sejong.sejongpeer.domain.study.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+public record StudyMatchingRequest(
+	@NotNull(message = "스터디 게시글 id를 입력해주세요.") Long studyId,
+	@NotNull(message = "수락/거절 하려는 지원자의 닉네임을 입력해주세요.") String applicantNickname,
+	@NotNull(message = "스터디 지원자를 수락하려면 true, 거절하려면 false로 요청해주세요.") boolean isAccept
+) {
+}
