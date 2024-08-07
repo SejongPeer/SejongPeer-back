eca81d82a75e7ac4b9e5559615256efd1bccf24b
Subin <rla0010101@gmail.com>
2024-08-07 23:20:02 +0900

    :recycle: facade class 생성 후 facade pattern 도입

---
+++ src/main/java/com/sejong/sejongpeer/domain/buddy/service/BuddyManagementFacade.java
@@ -0,0 +1,50 @@
+package com.sejong.sejongpeer.domain.buddy.service;
+
+import com.sejong.sejongpeer.domain.buddy.dto.request.BuddyRegistrationRequest;
+import com.sejong.sejongpeer.domain.buddy.dto.request.MatchingResultRequest;
+import com.sejong.sejongpeer.domain.buddy.dto.response.ActiveCustomersCountResponse;
+import com.sejong.sejongpeer.domain.buddy.dto.response.CompletedPartnerInfoResponse;
+import com.sejong.sejongpeer.domain.buddy.dto.response.MatchingPartnerInfoResponse;
+import com.sejong.sejongpeer.domain.buddy.dto.response.MatchingStatusResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BuddyManagementFacade {
+
+	private final BuddyService buddyService;
+	private final BuddyMatchingService buddyMatchingService;
+
+	public void registerBuddy(BuddyRegistrationRequest request) {
+		buddyService.registerBuddy(request);
+	}
+
+	public void cancelBuddy() {
+		buddyService.cancelBuddy();
+	}
+
+	public MatchingStatusResponse getBuddyMatchingStatusAndCount() {
+		return buddyService.getBuddyMatchingStatusAndCount();
+	}
+
+	public MatchingPartnerInfoResponse getBuddyMatchingPartnerDetails() {
+		return buddyService.getBuddyMatchingPartnerDetails();
+	}
+
+	public List<CompletedPartnerInfoResponse> getBuddyMatchedPartnerDetails() {
+		return buddyService.getBuddyMatchedPartnerDetails();
+	}
+
+	public ActiveCustomersCountResponse getCurrentlyActiveBuddyCount() {
+		return buddyService.getCurrentlyActiveBuddyCount();
+	}
+
+	public void updateBuddyMatchedStatus(MatchingResultRequest request) {
+		buddyMatchingService.updateBuddyMatchedStatus(request);
+	}
+}
