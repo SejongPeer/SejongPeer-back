2bf064d591b48807cc53d67ea7db8b4b22fdc1ec
Subin <rla0010101@gmail.com>
2024-08-13 10:37:47 +0900

    :sparkles: Redis Cache 사용 준비를 위한 세팅 완료

---
+++ src/main/java/com/sejong/sejongpeer/global/config/CacheConfig.java
@@ -0,0 +1,38 @@
+package com.sejong.sejongpeer.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class CacheConfig {
+
+	@Autowired
+	RedisConnectionFactory redisConnectionFactory;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
+	@Autowired
+	RedisConnectionFactory connectionFactory;
+
+	@Bean
+	public CacheManager redisCacheManager() {
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+		RedisCacheManager redisCacheManager = RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory)
+			.cacheDefaults(redisCacheConfiguration).build();
+
+		return redisCacheManager;
+	}
+}
