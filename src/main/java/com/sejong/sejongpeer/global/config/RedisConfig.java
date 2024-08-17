e9cfc0e1aada169246115e966ec75260a8de4e28
Subin <rla0010101@gmail.com>
2024-08-18 03:41:52 +0900

    :bug: nginx 502 error redis config annotation 추가

--- src/main/java/com/sejong/sejongpeer/global/config/RedisConfig.java
+++ src/main/java/com/sejong/sejongpeer/global/config/RedisConfig.java
@@ -11,11 +11,13 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableRedisRepositories
 public class RedisConfig {
 
 	private final RedisProperties redisProperties;
